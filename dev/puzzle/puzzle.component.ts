578357ab2c9a482fd954d7bb52c987aff6221d31
Peter Bankuti <peter.bankuti@techniatranscat.com>
2016-07-08 09:21:02 +0200

    Finished Puzzle

--- dev/puzzle/puzzle.component.ts
+++ dev/puzzle/puzzle.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 
 @Component({
     selector: 'my-puzzle',
@@ -6,9 +6,28 @@ import {Component} from 'angular2/core';
     styleUrls: ['dev/puzzle/puzzle.component.css']
 })
 
-export class PuzzleComponent {
+export class PuzzleComponent implements OnInit{
     switch1Number: number;
     switch2Number: number;
     switch3Number: number;
     switch4Number: number;
+
+    ngOnInit(){
+        this.switch1Number = Math.round(Math.random());
+        this.switch2Number = Math.round(Math.random());
+        this.switch3Number = Math.round(Math.random());
+        this.switch4Number = Math.round(Math.random());
+        // this.switch1Number = 1;
+        // this.switch2Number = 0;
+        // this.switch3Number = 1;
+        // this.switch4Number = 1;
+        console.log(
+            this.switch1Number,
+            this.switch2Number,
+            this.switch3Number,
+            this.switch4Number
+        )
+    }
+
+
 }
