476408fb7557c733366c5c2ae5a1f35882f5dd09
Padamban <peter.adam.b@gmail.com>
2016-07-09 17:14:02 +0200

    ngFor, ngSwitch

--- dev/structural-directives.component.ts
+++ dev/structural-directives.component.ts
@@ -12,12 +12,45 @@ import {Component} from 'angular2/core';
             </div>
             <br>
             
+            
             <h4>*ngIf</h4>
             <div *ngIf="number.value > 10" >Number is greater than 10.</div>
             
+            
+            
             <br><br>
             
-       
+            
+            
+            <h4>*ngFor</h4>
+            <div >
+                <!-- maybe '#...' will be 'let ...' in the future-->
+                <ul *ngFor="#item of list">
+                    <li>{{item}}</li>
+                </ul>
+                <br>
+                <ul *ngFor="#item of list, #i=index">
+                    <li>{{i+1}} {{item}}</li>
+                </ul>
+            </div>
+            
+            <br><br>
+            
+            <h4>[ngSwitch]</h4>
+            <!--It uses squere brakets because it doesnt changes the DOM, ... the * signals change in the DOM-->
+            <div >
+                Enter red, blue or green:           
+                <input type="text" #color (keyup)="0">   <!-- dummy event to update the ui-->
+                <br>
+                <div [ngSwitch]="color.value">
+                   <template [ngSwitchWhen]="'red'"> The color is RED.</template>
+                   <template [ngSwitchWhen]="'blue'"> The color is BLUE.</template>
+                   <template [ngSwitchWhen]="'green'"> The color is GREEN.</template>
+                   <template ngSwitchDefault> Don't know that color.</template>
+                </div>
+            </div>
+            
+            
     
     `,
     styles: [``],
@@ -28,5 +61,5 @@ import {Component} from 'angular2/core';
 
 export class StructuralDirectivesComponent
 {
-
+    list = ['Apple', 'Milk', 'Bread', 'Egg'];
 }
