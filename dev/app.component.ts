512cb84adc40b93ef33150c305fef2f25c14d2b6
Peter Bankuti <peter.bankuti@techniatranscat.com>
2016-07-11 16:58:56 +0200

    Cleaned the template

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,8 +3,8 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>App (root) component</h1>
-        <p>Here comes the child component...</p>
+        <h1>MASTER</h1>
+        
         `
 })
 export class AppComponent {
