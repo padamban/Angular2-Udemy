5137e480445acd1053e17194bd21465e347a9907
Peter Bankuti <peter.bankuti@techniatranscat.com>
2016-07-11 13:37:47 +0200

    Introduced form and validator mechanism into the "new item component"

--- dev/shopping/shopping-list-new-item.component.ts
+++ dev/shopping/shopping-list-new-item.component.ts
@@ -1,19 +1,25 @@
-import {Component, EventEmitter} from 'angular2/core';
+import {Component, EventEmitter, OnInit} from 'angular2/core';
 import {ShoppingListService} from "./shopping-list.service";
+import {ControlGroup, FormBuilder, Validators, Control} from "angular2/common";
 
 
 @Component ({
     selector: 'my-shopping-list-new-item',
     template: `
-        <div>
-            <label for="item-name">Name</label>
-            <input type="text" id="item-name" [(ngModel)]="item.name">
-        </div>
-        <div>
-            <label for="item-amt">Amount</label>
-            <input type="text" id="item-amt" [(ngModel)]="item.amount">
-        </div>
-        <button (click)="onClick()">Add item</button>
+        <form [ngFormModel]="myForm" (ngSubmit)="onSubmit()">
+            <div>
+                <label for="item-name">Name</label>
+                <input type="text" id="item-name" [(ngModel)]="item.name" [ngFormControl]="myForm.controls['itemName']">
+            </div>
+            <div>
+                <label for="item-amt">Amount</label>
+                <input type="text" id="item-amt" [(ngModel)]="item.amount" [ngFormControl]="myForm.controls['itemAmount']">
+            </div>
+            <button type="submit"  [disabled]="!myForm.valid">Add item</button>
+        </form>
+
+
+       
     
     `,
     styles: [`
@@ -26,20 +32,43 @@ import {ShoppingListService} from "./shopping-list.service";
     outputs: []
 })
 
-export class ShoppingListNewItemComponent
+export class ShoppingListNewItemComponent implements OnInit
 {
+    myForm: ControlGroup;
     item = {name: '', amount:0};
+
     // itemAdded = new EventEmitter<{name:string, amount:number}>();
+    // constructor(private _shoppingListService: ShoppingListService){}
 
-    constructor(private _shoppingListService: ShoppingListService){}
+    constructor(
+        private _shoppingListService: ShoppingListService,
+        private _formBuilder: FormBuilder
+    ){}
 
-    onClick(){
+    ngOnInit():any {
+        this.myForm = this._formBuilder.group({
+            'itemName': ['', Validators.required],
+            'itemAmount': ['', Validators.compose([
+                Validators.required,
+                greaterThanZero
+            ])],
+        })
+    }
+
+    onSubmit(){
         // this.itemAdded.emit(this.item);
-
         // this created two way data binding, --> pass only the values, not the object
         // this._shoppingListService.insertItem(this.item);
 
-
         this._shoppingListService.insertItem({name: this.item.name, amount: this.item.amount});
+    
+
+    }
+}
+
+// Custom validator
+function greaterThanZero(control:Control):{[s: string]:boolean}{
+    if(control.value <= 0){
+        return {notEnoug:true};
     }
 }
